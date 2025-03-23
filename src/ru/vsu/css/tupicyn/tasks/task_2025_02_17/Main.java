1c88653092c1f398aa3dffe81d1ff55e0c9ea0c0
Parnisha101 <dimatup40@gmail.com>
2025-03-23 17:02:01 +0300

    недоработанный таск

---
+++ src/ru/vsu/css/tupicyn/tasks/task_2025_02_17/Main.java
@@ -0,0 +1,32 @@
+package ru.vsu.css.tupicyn.tasks.task_2025_02_17;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, -2, -33, 4, -6, -6, -8, 10, -2, -3);
+        System.out.println(task(nums));
+    }
+
+    public static int task(List<Integer> list) {
+        int targetIndex = list.size() - 1;
+        int targetCounter = 0;
+        int counter = 0;
+
+        for (int currentIndex = 0; currentIndex < list.size(); currentIndex++) {
+            if (list.get(currentIndex) > 0) {
+                for (int nextIndex = currentIndex + 1; nextIndex < list.size(); nextIndex++) {
+                    if (list.get(nextIndex) < 0) counter++;
+                    else break;
+                }
+                if (targetCounter <= counter) {
+                    targetCounter = counter;
+                    targetIndex = currentIndex;
+                    counter = 0;
+                }
+            }
+        }
+
+        return list.get(targetIndex);
+    }
+}
